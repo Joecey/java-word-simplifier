9a2fca7f3e498e5a123e8b5f96bed946adb1afb3
Joe Linogao <linogaoj@tcd.ie>
2024-12-02 19:14:12 +0000

    Initial runner class

---
+++ src/ie/atu/sw/Runner.java
@@ -0,0 +1,8 @@
+package ie.atu.sw;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println("***** The Blazingly Fast and Cutting Edge Word Simplifier *****");
+        System.out.println("Created by Joe Linogao");
+    }
+}
